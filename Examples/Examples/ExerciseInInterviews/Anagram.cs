b6a96dd98022088d7ed173f507f2438dc1074c21
Jesus270192 <33634662+Jesus270192@users.noreply.github.com>
2022-07-07 17:41:54 -0500

    doing Angram DCX Technology

---
+++ Examples/Examples/ExerciseInInterviews/Anagram.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.ExerciseInInterviews
+{
+  public  class Anagram
+    {
+		public void DoAnagram()
+		{
+			List<string> words = new List<string>() { "roma", "abc", "amor", "cba", "mora", "aab", "ramo", "bba", "omar", "abb", "poder", "red", "pedro", "edr", "saunas", "def", "susana" };
+			List<string> aux = new List<string>() { "roma", "abc", "amor", "cba", "mora", "aab", "ramo", "bba", "omar", "abb", "poder", "red", "pedro", "edr", "saunas", "def", "susana" };
+			List<string> theseWordsAreAnagram, skipword;
+			string value1, value2;
+			theseWordsAreAnagram = skipword = new List<string>();
+
+			foreach (string value in words)
+			{
+				if (skipword.Count(s => s == value) > 0)
+				{
+					continue;
+				}
+
+				theseWordsAreAnagram = new List<string>();
+				skipword.Add(value);
+				aux.Remove(value);
+
+				value2 = String.Concat(value.OrderBy(c => c));
+
+				foreach (string valueAux in aux)
+				{
+					value1 = String.Concat(valueAux.OrderBy(c => c));
+
+
+					if (value1 == value2)
+					{
+						theseWordsAreAnagram.Add(valueAux);
+					}
+				}
+
+				if (theseWordsAreAnagram.Count() > 0)
+				{
+					Console.WriteLine(value + " " + String.Join(" ", theseWordsAreAnagram.ToArray()) + " ->>> Anagram");
+				}
+				else
+				{
+					Console.WriteLine(value + "-->>> No Anagram");
+				}
+
+				foreach (var item in theseWordsAreAnagram)
+				{
+					skipword.Add(item);
+					aux.Remove(item);
+				}
+			}
+		}
+	}
+}
--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -7,22 +7,26 @@ using Examples.WorkingArrays;
 
 namespace Examples
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            IntArray array = new IntArray(10, 2, 3);
-            DecimalArray decArr = new DecimalArray(decimal.One, decimal.Zero, decimal.MaxValue);
-            StringArray arrayString = new StringArray("Monica", "Chuy", "Chato");
-            StoreArray store = new StoreArray(new Store() { Name="Monica" }, new Store() { Name = "Chuy" }, new Store() { Name = "Chato" });
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IntArray array = new IntArray(10, 2, 3);
+			DecimalArray decArr = new DecimalArray(decimal.One, decimal.Zero, decimal.MaxValue);
+			StringArray arrayString = new StringArray("Monica", "Chuy", "Chato");
+			StoreArray store = new StoreArray(new Store() { Name = "Monica" }, new Store() { Name = "Chuy" }, new Store() { Name = "Chato" });
 
-            array.PrintArray();
-            decArr.PrintArray();
-            arrayString.PrintArray();
-            store.PrintArray();
+			Console.ReadLine();
+			array.PrintArray();
+			decArr.PrintArray();
+			arrayString.PrintArray();
+			store.PrintArray();
 
-            Console.Write("Press any key to exit");
-            Console.ReadLine();
-        }
-    }
+			Console.Write("Press any key to exit");
+			
+		}
+
+
+		
+	}
 }
